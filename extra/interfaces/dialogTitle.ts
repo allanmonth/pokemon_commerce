5b393cace8afc2d9ecfa0c519124b09261eb676b
Allan Monteiro <45175421+allanmonth@users.noreply.github.com>
2021-09-14 01:18:25 -0300

    Update dialogTitle.ts

--- extra/interfaces/dialogTitle.ts
+++ extra/interfaces/dialogTitle.ts
@@ -1,8 +1,8 @@
 import { WithStyles } from "@material-ui/core/styles";
 import React from "react";
-import { styles } from "../../styles/dialog";
+import { dialogCSS } from "../../styles/dialog";
 
-export interface DialogTitleProps extends WithStyles<typeof styles> {
+export interface DialogTitleProps extends WithStyles<typeof dialogCSS> {
     id: string;
     children: React.ReactNode;
     onClose: () => void;
